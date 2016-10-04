0d327306b9683b4d62d579fa78f317da13b33cba
Tchontk <tchontk@gmail.com>
2016-10-04 19:45:41 +0200

    Suppression du package insecure Création de méthode pour la manipulation des données

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,6 +6,43 @@
 import {
   Mongo
 } from 'meteor/mongo';
-
+import {
+  Meteor
+} from 'meteor/meteor';
+import {
+  check
+} from 'meteor/check';
 // le nom de l'export est "Tasks"
 export const Tasks = new Mongo.Collection('tasks');
+
+/**
+ * En supprimant le package insecure
+ * Il devient obligatoire de définir des méthodes pour les modifications des données
+ */
+Meteor.methods({
+  'tasks.insert' (text) {
+    check(text, String);
+    if (!this.userId) {
+      throw new Meteor.error("identification manquante")
+    }
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove' (taskId) {
+    check(taskId, String);
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked' (taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+    Tasks.update(taskId, {
+      $set: {
+        checked: setChecked
+      },
+    });
+  }
+})
